301d3689f320ec7ecd56408da100b1ca948fde3b
denizcetinnn <denizcetinnn@gmail.com>
2025-10-12 23:21:00 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/EmojiCrawler/',
   plugins: [react()],
 })
